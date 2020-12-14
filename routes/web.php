7bd94fef2fecfed662144e5aee41c482e99ce08f
tryanswer42 <tryanswer42@gmail.com>
2020-12-14 14:12:23 +0100

    Setting basic routes Generating UsersController.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,57 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//home page
+Route::get('/home', function () {
+    return view('home');
+});
+
+//register page
+Route::get('/register', function () {
+    return view('register');
+});
+
+//Register validation
+Route::get('/register/ok', function () {
+    return view('welcome');
+});
+
+//login page
+Route::get('/login', function () {
+    return view('login');
+});
+
+//loggedin user page
+Route::get('/users/{id}', 'UsersController@show');
+
+//contact Page
+Route::get('contact', function () {
+    return view('contact');
+});
+
+//FAQ
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+//about
+Route::get('/about', function () {
+    return view('about');
+});
+
+//Admin board
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+//Admin board after search
+Route::get('/admin/search', function () {
+    return view('admin');
+});
+
+
+//Articles
+Route::get('/article', function () {
+    return view('article');
+});
